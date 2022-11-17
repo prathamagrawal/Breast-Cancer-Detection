736e0c08b47e5b0a1993d17b1ca9992dd8ba2ae0
Pratham Agrawal <prathamagrawal1205@gmail.com>
2022-11-17 22:01:38 +0530

    Flask App

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import load_img
+import tensorflow as tf
 
 UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+
+batch_size = 32
+img_height = 224
+img_width = 224
+
+
 app = Flask(__name__)
 app.secret_key = "secret key1"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+testing='static/uploads/test.png'
 
 def allowed_file(filename): 
     return '.' in filename and \
@@ -34,6 +44,17 @@ def upload_image():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
+
+            # Loading the Model
+        savedModel = load_model('cancerModel.h5')
+        savedModel.summary()
+
+        
+        img1=load_img(testing)
+        img1=img1.resize((img_height, img_width))
+        input_arr = tf.keras.preprocessing.image.img_to_array(img1)
+        img = input_arr.reshape( -1,224, 224,3)
+        img.shape
 
         return render_template('index.html', filename=filename)
 
